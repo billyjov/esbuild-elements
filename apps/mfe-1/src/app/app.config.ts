af5072a426d67208da620131352f1ebc534120e9
Billy Lando <billy_lando@yahoo.com>
2025-01-29 01:20:39 +0100

    fix: use global zone

--- apps/mfe-1/src/app/app.config.ts
+++ apps/mfe-1/src/app/app.config.ts
@@ -1,10 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, NgZone, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (globalThis as any).ngZone ? { provide: NgZone, useValue: (globalThis as any).ngZone } : [],
     provideRouter(appRoutes),
   ],
 };
